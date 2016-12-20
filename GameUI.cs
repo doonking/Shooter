2336a6299dbfa3e56343f98e767ca39190dc36e0
Giant <d.kirtsana@hotmail.com>
2016-12-20 09:29:50 +0700

    Add files via upload

---
+++ GameUI.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameUI : MonoBehaviour {
+
+    public Image fadePlane;
+    public GameObject gameOverUI;
+
+    public RectTransform newWaveBanner;
+    public Text newWaveTitle;
+
+    Spawner spawner;
+
+	// Use this for initialization
+	void Start () {
+        FindObjectOfType<Player>().OnDeath += OnGameOver;
+	}
+
+    void Awake()
+    {
+        spawner = FindObjectOfType<Spawner>();
+        spawner.OnNewWave += OnNewWave;
+    }
+
+    void OnNewWave(int waveNumber)
+    {
+        string[] numbers = { "One", "Two", "Three", "Four", "Five" };
+        newWaveTitle.text = "Stage " + numbers[waveNumber - 1];
+
+        StartCoroutine(AnimateNewWaveBanner());
+    }
+    void OnGameOver()
+    {
+        StartCoroutine(Fade(Color.clear, Color.black, 1));
+        gameOverUI.SetActive(true);
+    }
+
+    IEnumerator AnimateNewWaveBanner()
+    {
+        float delayTime = 1f;
+        float speed = 2.5f;
+        float animatePercent = 0;
+        int dir = 1;
+
+        float endDelayTime = Time.time + 1 / speed + delayTime;
+        
+        while ( animatePercent >= 0)
+        {
+            animatePercent += Time.deltaTime * speed * dir;
+            
+            if (animatePercent >= 0)
+            {
+                
+                if(animatePercent >= 1)
+                {
+                    animatePercent = 1;
+                    if(Time.time > endDelayTime)
+                    {
+                        dir = -1;
+                    }
+                }
+
+                newWaveBanner.anchoredPosition = Vector2.up * Mathf.Lerp(-500, 0, animatePercent);
+                yield return null;
+            }
+        }
+    }
+
+    IEnumerator Fade(Color from, Color to, float time)
+    {
+        float speed = 1 / time;
+        float percent = 0;
+
+        while (percent < 1)
+        {
+            percent += Time.deltaTime * speed;
+            fadePlane.color = Color.Lerp(from, to, percent);
+            yield return null;
+        }
+    }
+
+    //UI Input
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Gun.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+    public enum FireMode { Auto, Burst, Single};
+    public FireMode fireMode;
+
+    public Transform[] projectileSpawn;
+    public Projectile projectile;
+    public float msBetweenShots = 100;
+    public float muzzleVelocity = 35;
+    public int burstCount;
+
+    [Header("Effects")]
+    public Transform shell;
+    public Transform shellEjection;
+    public AudioClip shootAudio;
+    MuzzleFlash muzzleflash;
+    float nextShotTime;
+
+    bool triggerReleasedSinceLastShot;
+    int shotsRemainingInBurst;
+
+     void Start()
+    {
+        muzzleflash = GetComponent<MuzzleFlash>();
+        shotsRemainingInBurst = burstCount;
+    } 
+
+    float nextShortTime;
+
+     void Shoot()
+    {
+    if (Time.time > nextShortTime)
+        {
+            if(fireMode == FireMode.Burst)
+            {
+                if (shotsRemainingInBurst == 0)
+                {
+                    return;
+                }
+                shotsRemainingInBurst--;
+            } else if (fireMode == FireMode.Single)
+            {
+                if (!triggerReleasedSinceLastShot)
+                {
+                    return;
+                }
+            }
+
+            for(int i =0; i< projectileSpawn.Length; i++)
+            {
+
+            
+            nextShortTime = Time.time + msBetweenShots / 1000;
+            Projectile newProjecttile = Instantiate(projectile, projectileSpawn[i].position, projectileSpawn[i].rotation) as Projectile;
+            newProjecttile.SetSpeed(muzzleVelocity);
+            }
+
+            Instantiate(shell, shellEjection.position, shellEjection.rotation);
+            muzzleflash.Activate();
+
+            AudioManager.instance.PlaySound(shootAudio, transform.position);
+        }
+    }
+
+    public void OnTriggerHold()
+    {
+        Shoot();
+        triggerReleasedSinceLastShot = false;
+    }
+
+        public void OnTriggerRelease()
+    {
+        triggerReleasedSinceLastShot = true;
+        shotsRemainingInBurst = burstCount;
+    }
+}
